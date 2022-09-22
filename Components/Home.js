af3c7c8e89620a9dc57eac1badc6371899ccc53e
Branden Jones <branden.wayne.jones@gmail.com>
2022-09-21 22:14:45 -0400

    Branden Jones Creating Repo

---
+++ Components/Home.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+function Home() {
+    return (
+        <div>
+            <header>
+                <h1>Home</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Home
--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import  ReactDOM  from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import AvengerList from '../Components/AvengersList';
+import { useParams } from 'react-router-dom';
+import App from './App';
+import AvengerList from '../Components/AvengersList';
+import Home from '../Components/Home';
+
+// function Home(props) {
+//   return <h3>This Is Home</h3>
+// }
+
+function Avenger({ hero }) {
+  
+  const params = useParams();
+
+  const heros = hero.find(item = item.id === Number(params.hero));
+
+  return (
+   <div className='character-card'>
+    <header className='App-Header'>
+       <h2>Hey Youz Guyz...gender-inclusive</h2>
+       <h2>{heros.name}</h2>
+       <p>{heros.nickname}</p>
+       <p>{heros.description}</p>
+       <img src={heros.thumbnail} alt="random avengers img" />
+    </header>
+   </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Route exact path='/avengers/:hero'>
+              <Avenger key={hero.id} hero={hero} />
+          </Route>
+          <Route exact path='/avengers/'>
+              <AvengerList hero={hero} />
+          </Route>
+          <Route exact path='/' component={Home} />
     </div>
   );
 }
